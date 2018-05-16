7675870054bb6d664b1743a26a7db1c1bb440015
Aivuo <rioneda1@gmail.com>
2018-05-16 13:59:36 +0200

    Init och grund

---
+++ MVC Site 2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Site_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "You start here";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "There might be some stuff about stuff here";
+            return View();
+        }
+
+        public ActionResult Stuff()
+        {
+            ViewBag.Message = "Stuff gets put here";
+            return View();
+        }
+
+        public ActionResult TheStuff()
+        {
+            ViewBag.Message = "THE stuff gets put here";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact information";
+            return View();
+        }
+    }
+}
