d06d19723a01d069ac55ac8010eb08609dfd18e8
Aivuo <rioneda1@gmail.com>
2018-05-17 11:13:40 +0200

    FeverCheck is working

--- MVC Site 2/App_Start/RouteConfig.cs
+++ MVC Site 2/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace MVC_Site_2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "TemperatureCheck",
+                url: "FeverCheck/{temperature}",
+                defaults: new { controller = "Home", action = "FeverCheck", temperature = 37}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MVC Site 2/Controllers/HomeController.cs
+++ MVC Site 2/Controllers/HomeController.cs
@@ -33,5 +33,44 @@ namespace MVC_Site_2.Controllers
             ViewBag.Message = "Contact information";
             return View();
         }
+
+        [HttpGet]
+        public ActionResult FeverCheck()
+        {
+            ViewBag.Message = "FeverStuff";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FeverCheck(int temperature)
+        {
+            //bool fever = false;
+            //bool hypothermia = false;
+            string haveFever;
+
+            if (temperature > 38)
+            {
+                haveFever = "Yes you have a fever.";
+            } else if(temperature > 35)
+            {
+                haveFever = "No, you are perfectly fine.";
+            }
+            else if (temperature > 32)
+            {
+                haveFever = "No but you have a mild case of Hypothermia. Drink something warm and contact a doctor.";
+            }
+            else if (temperature > 28)
+            {
+                haveFever = "You have severe Hypothermia. Get to a doctor now!";
+            } else
+            {
+                haveFever = "How did you even write this? You should be unconcious!";
+            }
+
+            ViewBag.haveFever = haveFever;
+
+            ViewBag.Message = "FeverStuff with temperature";
+            return View();
+        }
     }
 }
